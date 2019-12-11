2315d18f399b77714a46d695a5a5649a60890637
salanho <espiritosanto.gabriel@gmail.com>
2019-12-11 01:43:55 -0300

    pronto

--- src/pages/Inbox/styles.js
+++ src/pages/Inbox/styles.js
@@ -11,9 +11,6 @@ export const Container = styled.div`
   margin-top: 0;
   padding: 1% 0.5%;
   box-shadow: 0 8px 10px 1px rgba(0,0,0,0.14), 0 3px 14px 2px rgba(0,0,0,0.12), 0 5px 5px -3px rgba(0,0,0,0.20);
-
-
-
 `;
 
 export const Title = styled.h1`
